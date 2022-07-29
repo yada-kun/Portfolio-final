3177f64b370e431ea5df844e4717eff672716d27
James <anunciojames14@gmail.com>
2022-07-29 19:10:49 +0800

    Fix: added git hub repo

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -18,6 +18,7 @@ const Portfolio = () => {
                     <p>HTML & CSS</p>
                     <p className="my-5">This is a static website using HTML & CSS. The website is mobile responsive and is a demonstration of how to create a simple website using basic HTML & CSS</p>
                     <button className="bg-blue-500 w-[120px] text-white hover:bg-red-700 h-[30px]"><a href="https://fluffy-breadpan.herokuapp.com/" >Visit Site</a> </button>
+                    <button className="bg-blue-500 w-[120px] mx-5 text-white hover:bg-red-700 h-[30px]"><a href="https://github.com/yada-kun/Project-1" >Git Hub</a> </button>
                 </div>
                 <div className="w-[500px] h-[300px] text-left">
                     <img className=" object-cover w-full h-full text-left" src={Project1} alt="Project1"/>
@@ -36,6 +37,7 @@ const Portfolio = () => {
                     <p>HTML, CSS & Javascript</p>
                     <p className="my-5">This is a Game Project that applies our learnings using Javascript. The mechanics of the game is simple. Flip and compare the cards and the highest poins win</p>
                     <button className="bg-blue-500 w-[120px] text-white hover:bg-red-700 h-[30px]"><a href="https://memory-game14.herokuapp.com/" target="_blank" rel="noreferrer">Visit Site</a> </button>
+                    <button className="bg-blue-500 w-[120px] mx-5 text-white hover:bg-red-700 h-[30px]"><a href="https://github.com/yada-kun/Project-2" >Git Hub</a> </button>
                 </div>
             </div>
         </div>
@@ -45,8 +47,9 @@ const Portfolio = () => {
                 <div className="w-[550px] text-left p-5">
                     <h4 className="text-[18px] font-bold"><span className="text-[#0bceaf]">Pokemon Pokedex</span> - App</h4>
                     <p>HTML,CSS, Javascript & React</p>
-                    <p className="my-5">This is a static website using HTML & CSS. The website is mobile responsive and is a demonstration of how to create a simple website using basic HTML & CSS</p>
+                    <p className="my-5">This is a static website using HTML, CSS & React. This is a mini pokedex website that gathers all the data of all most all pokemons. User just needs to choose a pokemon type category and it will display all the pokemons in that specific type.</p>
                     <button className="bg-blue-500 w-[120px] text-white hover:bg-red-700 h-[30px]"><a href="https://pokemon-pokedexapp.herokuapp.com/pokemon" target="_blank" rel="noreferrer" >Visit Site</a> </button>
+                    <button className="bg-blue-500 w-[120px] mx-5 text-white hover:bg-red-700 h-[30px]"><a href="https://github.com/yada-kun/Project-3" >Git Hub</a> </button>
                 </div>
                 <div className="w-[500px] h-[300px] text-left">
                     <img className="object-cover w-full h-full text-left" src={Project3} alt="Project1"/>
@@ -65,6 +68,7 @@ const Portfolio = () => {
                     <p>HTML,CSS, Javascript & React</p>
                     <p className="my-5">I have created a simple Task Applciation using React.js. Where it can add, delete and edit tasks just like a normal Task App</p>
                     <button className="bg-blue-500 w-[120px] text-white hover:bg-red-700 h-[30px]"><a href="https://todolist-app14.herokuapp.com/" target="_blank" rel="noreferrer">Visit Site</a> </button>
+                    <button className="bg-blue-500 w-[120px] mx-5 text-white hover:bg-red-700 h-[30px]"><a href="https://github.com/yada-kun/Project-4" >Git Hub</a> </button>
                 </div>
             </div>
         </div>
@@ -76,6 +80,7 @@ const Portfolio = () => {
                     <p>HTML,CSS, Javascript & React</p>
                     <p className="my-5">This is an E-commerce website app created using react. This has no back-end yet but we can still perform a simple front end add to cart function. This website is Black Pink inspired</p>
                     <button className="bg-blue-500 w-[120px] text-white hover:bg-red-700 h-[30px]"><a href="https://bp-ecommerce-app.herokuapp.com/" target="_blank" rel="noreferrer">Visit Site</a> </button>
+                    <button className="bg-blue-500 w-[120px] mx-5 text-white hover:bg-red-700 h-[30px]"><a href="https://github.com/yada-kun/Project-5" >Git Hub</a> </button>
                 </div>
                 <div className="w-[500px] h-[300px] text-left">
                     <img className="object-cover w-full h-full text-left" src={Project5} alt="Project1"/>
@@ -94,6 +99,7 @@ const Portfolio = () => {
                     <p>HTML,CSS, Javascript, React, Node, Express & MongoDB</p>
                     <p className="my-5">This is a soon to be filipino novel website where filipino artist can showcase their talents in creating web novels.</p>
                     <button className="bg-blue-500 w-[120px] text-white hover:bg-red-700 h-[30px]"><a href="https://cloudsky-webnovel.herokuapp.com/"  target="_blank" rel="noreferrer">Visit Site</a> </button>
+                    <button className="bg-blue-500 w-[120px] mx-5 text-white hover:bg-red-700 h-[30px]"><a href="https://github.com/yada-kun/Project-6" >Git Hub</a> </button>
                 </div>
             </div>
         </div>
@@ -105,6 +111,7 @@ const Portfolio = () => {
                     <p>HTML,CSS, Javascript, React, Node, Express & MongoDB</p>
                     <p className="my-5">This is a full travel application exclusive here on the Philipphines. This website is created using MERN Stack and it can function like any normal travel app.</p>
                     <button className="bg-blue-500 w-[120px] text-white hover:bg-red-700 h-[30px]"><a href="https://62e2dd6e4844471ccf13f227--lively-gumption-2fffc2.netlify.app/"  target="_blank" rel="noreferrer">Visit Site</a> </button>
+                    <button className="bg-blue-500 w-[120px] mx-5 text-white hover:bg-red-700 h-[30px]"><a href="https://github.com/yada-kun/Project-7-fe" >Git Hub</a> </button>
                 </div>
                 <div className="w-[500px] h-[300px] text-left">
                     <img className="object-cover w-full h-full text-left" src={Project7} alt="Project1"/>
